660c2da3eca4964ad0b00ca0300b8ac4833badb9
Jerome Duquennoy <jerome@duquennoy.fr>
2015-08-13 22:13:44 +0200

    iOS target version set to iOS9 to be able to compile with xCode 7 beta 5

---
+++ Log4Swift-iOS/log4swift-ios.h
@@ -0,0 +1,19 @@
+//
+//  log4swift-ios.h
+//  log4swift-ios
+//
+//  Created by jerome on 04/08/2015.
+//  Copyright © 2015 jerome. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for log4swift-ios.
+FOUNDATION_EXPORT double log4swift_iosVersionNumber;
+
+//! Project version string for log4swift-ios.
+FOUNDATION_EXPORT const unsigned char log4swift_iosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <log4swift_ios/PublicHeader.h>
+
+
