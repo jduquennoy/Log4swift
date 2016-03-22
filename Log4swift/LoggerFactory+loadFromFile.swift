90d3e2e265983879a7b6c0087139e924d7ec4802
Jerome Duquennoy <jerome@duquennoy.fr>
2016-03-22 21:52:33 +0100

    Fixed continuously reloading observed configuration file once modified, tilde expanded in configuration file path

--- Log4swift/LoggerFactory+loadFromFile.swift
+++ Log4swift/LoggerFactory+loadFromFile.swift
@@ -28,161 +28,162 @@ extension LoggerFactory : FileObserverDelegate {
     case Loggers = "Loggers"
     case RootLogger = "RootLogger"
     case Identifier = "Identifier"
-  };
+  }
   
   /// Load configuration file in plist format.
   /// - parameter filePath: the path to the file to load
   /// - parameter autoReload: true if the configuration file should be reloaded automatically when modified. If file does not exist, it will be loaded when created. Only one file can be auto-reloaded at a time. If a second file is marked as such, the first one will no longer be.
   public func readConfigurationFromPlistFile(filePath: String, autoReload: Bool = false, reloadInterval: NSTimeInterval = 5.0) throws {
-    let configurationNSDictionary = NSDictionary(contentsOfFile: filePath);
+    let expandedFilePath = (filePath as NSString).stringByExpandingTildeInPath
+    let configurationNSDictionary = NSDictionary(contentsOfFile: expandedFilePath)
     if let configurationDictionary = configurationNSDictionary as? Dictionary<String, AnyObject> {
-      try self.readConfiguration(configurationDictionary);
+      try self.readConfiguration(configurationDictionary)
     }
     if autoReload {
-      self.configurationFileObserver = FileObserver(filePath: filePath, poolInterval: reloadInterval);
-      self.configurationFileObserver?.delegate = self;
+      self.configurationFileObserver = FileObserver(filePath: expandedFilePath, poolInterval: reloadInterval)
+      self.configurationFileObserver?.delegate = self
     }
   }
   
   /// Reads a whole configuration from the given dictionary.
   public func readConfiguration(configurationDictionary: Dictionary<String, AnyObject>) throws {
-    try self.readConfigurationToTupple(configurationDictionary);
+    try self.readConfigurationToTupple(configurationDictionary)
   }
   
   // This internal method returns all created objects in a tupple, to make testing easier.
   // The public version does not return a tupple, an thus is compatible with Objective-C.
   internal func readConfigurationToTupple(configurationDictionary: Dictionary<String, AnyObject>) throws -> (Array<Formatter>, Array<Appender>, Array<Logger>) {
-    var formatters = Array<Formatter>();
-    var appenders = Array<Appender>();
-    var loggers = Array<Logger>();
+    var formatters = Array<Formatter>()
+    var appenders = Array<Appender>()
+    var loggers = Array<Logger>()
     
     // Formatters
     if let formattersArray = configurationDictionary[DictionaryKey.Formatters.rawValue] as? Array<Dictionary<String, AnyObject>> {
       for currentFormatterDefinition in formattersArray {
-        let formatter = try processFormatterDictionary(currentFormatterDefinition);
-        formatters.append(formatter);
+        let formatter = try processFormatterDictionary(currentFormatterDefinition)
+        formatters.append(formatter)
       }
     }
     
     // Appenders
     if let appendersArray = configurationDictionary[DictionaryKey.Appenders.rawValue] as? Array<Dictionary<String, AnyObject>> {
       for currentAppenderDefinition in appendersArray {
-        let appender = try processAppenderDictionary(currentAppenderDefinition, formatters: formatters);
-        appenders.append(appender);
+        let appender = try processAppenderDictionary(currentAppenderDefinition, formatters: formatters)
+        appenders.append(appender)
       }
     }
     
     // Root logger
     if let rootLoggerDictionary = configurationDictionary[DictionaryKey.RootLogger.rawValue] as? Dictionary<String, AnyObject> {
-      try self.rootLogger.updateWithDictionary(rootLoggerDictionary, availableAppenders: appenders);
+      try self.rootLogger.updateWithDictionary(rootLoggerDictionary, availableAppenders: appenders)
     } else if configurationDictionary[DictionaryKey.RootLogger.rawValue] != nil {
-      throw NSError.Log4swiftErrorWithDescription("The '\(DictionaryKey.RootLogger.rawValue)' parameter should be a dictionary");
+      throw NSError.Log4swiftErrorWithDescription("The '\(DictionaryKey.RootLogger.rawValue)' parameter should be a dictionary")
     }
     
     // Loggers
     if let loggersArray = configurationDictionary[DictionaryKey.Loggers.rawValue] as? Array<Dictionary<String, AnyObject>> {
       let sortedLoggersArray = loggersArray.sort({ (a, b) -> Bool in
         do {
-          let identifierA: String = try self.identifierFromConfigurationDictionary(a);
-          let identifierB: String = try self.identifierFromConfigurationDictionary(b);
-          return identifierA.characters.count < identifierB.characters.count;
+          let identifierA: String = try self.identifierFromConfigurationDictionary(a)
+          let identifierB: String = try self.identifierFromConfigurationDictionary(b)
+          return identifierA.characters.count < identifierB.characters.count
         } catch {
-          return false;
+          return false
         }
-      });
+      })
       
       for currentLoggerDefinition in sortedLoggersArray {
         let logger = try processLoggerDictionary(currentLoggerDefinition, appenders: appenders)
-        loggers.append(logger);
-        try registerLogger(logger);
+        loggers.append(logger)
+        try registerLogger(logger)
       }
     }
     
-    return (formatters, appenders, loggers);
+    return (formatters, appenders, loggers)
   }
   
   private func processFormatterDictionary(dictionary: Dictionary<String, AnyObject>) throws -> Formatter {
-    let identifier = try identifierFromConfigurationDictionary(dictionary);
-    let formatter: Formatter;
+    let identifier = try identifierFromConfigurationDictionary(dictionary)
+    let formatter: Formatter
     if let className = dictionary[DictionaryKey.ClassName.rawValue] as? String {
       if let formatterType = formatterForClassName(className) {
-        formatter = formatterType.init(identifier);
-        try formatter.updateWithDictionary(dictionary);
+        formatter = formatterType.init(identifier)
+        try formatter.updateWithDictionary(dictionary)
       } else {
-        throw NSError.Log4swiftErrorWithDescription("Unknown formatter class '\(className)' for formatter '\(identifier)'");
+        throw NSError.Log4swiftErrorWithDescription("Unknown formatter class '\(className)' for formatter '\(identifier)'")
       }
     } else {
-      throw NSError.Log4swiftErrorWithDescription("Missing '\(DictionaryKey.ClassName.rawValue) parameter for formatter '\(identifier)'");
+      throw NSError.Log4swiftErrorWithDescription("Missing '\(DictionaryKey.ClassName.rawValue) parameter for formatter '\(identifier)'")
     }
     
-    return formatter;
+    return formatter
   }
   
   private func formatterForClassName(className: String) -> Formatter.Type? {
-    let type: Formatter.Type?;
+    let type: Formatter.Type?
     switch(className) {
     case "PatternFormatter":
-      type = PatternFormatter.self;
+      type = PatternFormatter.self
     default:
-      type = nil;
+      type = nil
     }
-    return type;
+    return type
   }
 
   private func processAppenderDictionary(dictionary: Dictionary<String, AnyObject>, formatters: Array<Formatter>) throws -> Appender {
-    let identifier = try identifierFromConfigurationDictionary(dictionary);
-    let appender: Appender;
+    let identifier = try identifierFromConfigurationDictionary(dictionary)
+    let appender: Appender
     if let className = dictionary[DictionaryKey.ClassName.rawValue] as? String {
       if let appenderType = appenderForClassName(className) {
-        appender = appenderType.init(identifier);
-        try appender.updateWithDictionary(dictionary, availableFormatters: formatters);
+        appender = appenderType.init(identifier)
+        try appender.updateWithDictionary(dictionary, availableFormatters: formatters)
       } else {
-        throw NSError.Log4swiftErrorWithDescription("Unknown appender class '\(className)' for appender '\(identifier)'");
+        throw NSError.Log4swiftErrorWithDescription("Unknown appender class '\(className)' for appender '\(identifier)'")
       }
     } else {
       throw NSError.Log4swiftErrorWithDescription("Missing '\(DictionaryKey.ClassName.rawValue) parameter for appender '\(identifier)'")
     }
     
-    return appender;
+    return appender
   }
   
   private func appenderForClassName(className: String) -> Appender.Type? {
-    let type: Appender.Type?;
+    let type: Appender.Type?
     switch(className.lowercaseString) {
     case "stdoutappender":
-      type = StdOutAppender.self;
+      type = StdOutAppender.self
     case "fileappender":
-      type = FileAppender.self;
+      type = FileAppender.self
     case "nsloggerappender":
-      type = NSLoggerAppender.self;
+      type = NSLoggerAppender.self
     case "nslogappender":
-      type = NSLogAppender.self;
+      type = NSLogAppender.self
     case "aslappender":
-      type = ASLAppender.self;
+      type = ASLAppender.self
     default:
-      type = nil;
+      type = nil
     }
-    return type;
+    return type
   }
 
   private func processLoggerDictionary(dictionary: Dictionary<String, AnyObject>, appenders: Array<Appender>) throws -> Logger {
-    let identifier = try identifierFromConfigurationDictionary(dictionary);
-    let logger = self.getLogger(identifier);
+    let identifier = try identifierFromConfigurationDictionary(dictionary)
+    let logger = self.getLogger(identifier)
     
-    try logger.updateWithDictionary(dictionary, availableAppenders: appenders);
+    try logger.updateWithDictionary(dictionary, availableAppenders: appenders)
     
-    return logger;
+    return logger
   }
   
   private func identifierFromConfigurationDictionary(configurationDictionary: Dictionary<String, AnyObject>) throws -> String {
-    let identifier: String;
+    let identifier: String
     if let safeIdentifier = configurationDictionary[DictionaryKey.Identifier.rawValue] as? String {
       if(safeIdentifier.isEmpty) {
-        throw NSError.Log4swiftErrorWithDescription("Empty '\(DictionaryKey.Identifier.rawValue)' parameter in dictionary");
+        throw NSError.Log4swiftErrorWithDescription("Empty '\(DictionaryKey.Identifier.rawValue)' parameter in dictionary")
       }
-      identifier = safeIdentifier;
+      identifier = safeIdentifier
     } else {
-      throw NSError.Log4swiftErrorWithDescription("Missing '\(DictionaryKey.Identifier.rawValue)' parameter in dictionary");
+      throw NSError.Log4swiftErrorWithDescription("Missing '\(DictionaryKey.Identifier.rawValue)' parameter in dictionary")
     }
     
     return identifier
@@ -190,8 +191,8 @@ extension LoggerFactory : FileObserverDelegate {
   
   public func fileChanged(atPath: String) {
     do {
-      NSLog("file \(atPath) changed, reloadging");
-      try self.readConfigurationFromPlistFile(atPath);
+      NSLog("file \(atPath) changed, reloadging")
+      try self.readConfigurationFromPlistFile(atPath)
     } catch (let error){
       // We dont't want to throw here, as it might be a temporary error until next edit.
       // So We just print out the error to the console, with description.
